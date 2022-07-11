3de8f53db101ee2484e753b5190a485034f6f948
bryanreyes28 <bryan28reyesirahola@gmail.com>
2022-07-10 23:59:52 -0400

    uploading files

---
+++ src/components/about/About.stories.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+import About from "./About"; 
+export default { 
+  title: "BryanVReyesOrtizIrahola/app/About", 
+  component: About, 
+}; 
+
+const Template = (args) => <About {...args} />; 
+export const about= Template.bind({}); 
---
+++ src/components/home/Home.stories.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+import Home from "./Home"; 
+export default { 
+  title: "BryanVReyesOrtizIrahola/app/Home", 
+  component: Home, 
+}; 
+
+const Template = (args) => <Home {...args} />; 
+export const home= Template.bind({}); 
